3a45dca9a3974b536520cd860c61fb1c2a47e464
Gabriel Durante <gabriel@ditech.com.br>
2018-05-03 14:14:35 -0300

    create comments table

---
+++ database/migrations/2018_05_03_170859_create_comments_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('project_id')
+                ->unsigned();
+
+            $table->integer('task_id')
+                ->unsigned();
+
+            $table->text('comment');
+
+            $table->timestamps();
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+
+            $table->foreign('task_id')
+                ->references('id')
+                ->on('tasks')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
